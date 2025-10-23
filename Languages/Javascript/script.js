15471daa69f7ebcf1e1fdaca1bb49ee022950bf5
Harshit Tripathi <htripathi9324@gmail.com>
2025-10-23 14:25:58 +0530

    Create script.js

---
+++ Languages/Javascript/script.js
@@ -0,0 +1,6 @@
+for (let i = 0; ; i++) {
+    console.log("The Number is: ",i);
+}
+
+// This loop will run indefinitely because there is no terminating condition.
+// To terminate the loop, Press ctrl + C in the terminal where the script is running.
